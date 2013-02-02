ff0838782e1806f859af4a5a69ba7b896a213da9
Garrett Robinson <garrett.f.robinson@gmail.com>
2013-02-02 12:46:12 -0800

    Implement karma counting feature

--- features/karma.js
+++ features/karma.js
@@ -1,9 +1,44 @@
 /* Basic re to match IRC nicks */
-var irc_nick_re = "([A-Za-z0-9\[\]\{\}\-\\\^\<]+)";
+var irc_nick_re = "([A-Za-z0-9]+)";
 
 var Karma = function(client) {
+  // DEBUG
   client.addListener('message', function (from, to, message) {
     console.log(from + ' => ' + to + ': ' + message);
+  });
+
+  var karma_scores = {};
+
+  client.addListener('message#', function(nick, to, text) {
+    get_karma_re = new RegExp('^karma ' + irc_nick_re + '$');
+    var match = get_karma_re.exec(text);
+    if(match) {
+      nick = match[1];
+      if(nick in karma_scores) {
+        response = nick + " has " + karma_scores[nick] + " karma.";
+      } else {
+        response = nick + " has no karma.";
+      }
+      client.say(to, response);
+    }
+  });
+
+  client.addListener('message#', function(nick, to, text) {
+    inc_karma_re = new RegExp('^' + irc_nick_re + '\\+\\+$');
+    var match = inc_karma_re.exec(text);
+    if(match) {
+      nick = match[1];
+      (nick in karma_scores) ? karma_scores[nick]++ : karma_scores[nick] = 1;
+    }
+  });
+
+  client.addListener('message#', function(nick, to, text) {
+    dec_karma_re = new RegExp('^' + irc_nick_re + '\\-\\-$');
+    var match = dec_karma_re.exec(text);
+    if(match) {
+      nick = match[1];
+      (nick in karma_scores) ? karma_scores[nick]-- : karma_scores[nick] = -1;
+    }
   });
 };
 
